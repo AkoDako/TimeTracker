664c65a5ed626af23d16d3ee8b6d488c068c4310
AkoDako <63671514+AkoDako@users.noreply.github.com>
2022-03-28 01:59:05 -0400

    changed to track urls

--- background.js
+++ background.js
@@ -1,10 +1,11 @@
 /*Code property of Lanney Wang*/
 
 chrome.tabs.onActivated.addListener( async function getCurrentTab() {
-  let queryOptions = { active: true, currentWindow: true };
+  let queryOptions = { active: true, lastFocusedWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
   //console.log(tab.title);
-  let url = new URL(tab.url);
+  console.log(tab[0].url);
+  let url = new URL(tab[0].url);
   let domain = url.domain;
   console.log(domain);
   chrome.storage.sync.set({ "recentlyVisited": domain }, function() {});
@@ -17,25 +18,25 @@ chrome.tabs.onActivated.addListener( async function getCurrentTab() {
     delayInMinutes: 1,
     periodInMinutes: 1
   });
-  chrome.storage.sync.get( "recentlyVisited", function (lastVisited) {
+  /*chrome.storage.sync.get( "recentlyVisited", function (lastVisited) {
     console.log(lastVisited.recentlyVisited);
-  });
+  });*/
   
 });
 
-chrome.alarms.onAlarm.addListener(function(alarm) {
-  async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    let currentTab = tab.title;
-
-  }
+chrome.alarms.onAlarm.addListener(async function(alarm) {
   if (alarm.name === "1min") {
-    let last = "";
+    let currentDomain;
+    let tab = getCurrentTab();
+    let url = new URL(tab[0].url);
+    currentDomain = url.domain;
+    let last;
     chrome.storage.sync.get( "recentlyVisited", function (lastVisited) {
       last = lastVisited;
-      console.log(last);
-      iif ()
+      console.log(`last: ${last} current domain: ${currentDomain}`);
+      if (last == currentDomain) {
+        console.log("domains match");
+      }
     });
   }
 });
@@ -45,13 +46,11 @@ first time: on activated?  something like that
 
 switching tabs:
 
-
-
-
 */
+/*
 async function getCurrentTab() {
   let queryOptions = { active: true, currentWindow: true };
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
 };
-
+*/
